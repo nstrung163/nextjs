6b9e3df2374c920c4aaecc29650f6164c625fc24
Trung Nguyen <nstrung163@gmail.com>
2023-05-27 19:42:11 +0700

    Dynamic component without SSR(server side rendering)

---
+++ components/common/header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Header() {
+  console.log("Render header");
+  return <div>Header</div>;
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,15 @@
+// import Header from "@/components/common/header";
+import dynamic from "next/dynamic";
 import React from "react";
 
+const Header = dynamic(() => import("@/components/common/header"), {
+  ssr: false,
+});
 export default function about() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <Header />
+    </div>
+  );
 }

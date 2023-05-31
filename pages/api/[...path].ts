979ec45cf1ed891ae136f024f52c71bc3bb8d29b
Trung Nguyen <nstrung163@gmail.com>
2023-06-01 06:50:14 +0700

    Fix warning when changing the port

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -14,12 +14,17 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  // Do not send the cookie to server
-  req.headers.cookie = "";
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+  return new Promise((resolve) => {
+    // Do not send the cookie to server
+    req.headers.cookie = "";
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+    // res.status(200).json({ name: "PATH - Match all path" });
+    proxy.once("proxyRes", () => {
+      resolve(true);
+    });
   });
-  // res.status(200).json({ name: "PATH - Match all path" });
 }
